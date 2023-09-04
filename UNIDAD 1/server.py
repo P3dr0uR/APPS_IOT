114ead23ec9ca31716913d6ec16f78e9cc38fac4
P3dr0uR <100330982+P3dr0uR@users.noreply.github.com>
2023-09-04 15:58:22 -0600

    Unidad 1

---
+++ UNIDAD 1/server.py
@@ -0,0 +1,51 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+contador = 11
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def _set_response(self, content_type="text/plain"):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_response()
+        respuesta = "El valor es: " + str(contador)
+        self.wfile.write(respuesta.encode())
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+        
+        body_json = json.loads(post_data.decode())
+        
+        global contador
+        
+        if(body_json['action'] == 'asc'):
+            contador += 1
+        elif(body_json['action'] == 'desc'):
+            contador -= 1 
+
+        # Crear una respuesta JSON con el valor actualizado de contador
+        response_data = json.dumps({"message": "Received POST data", "contador": contador, "status": "OK"})
+
+        # Configurar la respuesta HTTP y enviarla al cliente
+        self._set_response("application/json")
+        self.wfile.write(response_data.encode())
+
+        # Print the complete HTTP request
+        print("\n----- Incoming POST Request -----")
+        print(f"Requestline: {self.requestline}")
+        print(f"Headers:\n{self.headers}")
+        print(f"Body:\n{post_data.decode()}")
+        print("-------------------------------")
+
+def run_server(server_class=HTTPServer, handler_class=MyHTTPRequestHandler, port=7800):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
