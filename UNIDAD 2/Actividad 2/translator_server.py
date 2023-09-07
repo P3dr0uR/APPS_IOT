0a60b200eea9187ff44ee33444e167a6ec808a17
P3dr0uR <100330982+P3dr0uR@users.noreply.github.com>
2023-09-07 14:45:32 -0600

    translator server

--- UNIDAD 2/Actividad 2/translator_server.py
+++ UNIDAD 2/Actividad 2/translator_server.py
@@ -1,18 +1,29 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+import requests
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+X_RAPIDAPI_KEY = os.getenv("X_RAPIDAPI_KEY")
+
+
 contador = 11
 
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
-  def _set_response(self, content_type="text/plain"):
-    self.send_response(200)
+  def _set_response(self, content_type="text/plain",status_code=200):
+    self.send_response(status_code)
     self.send_header("Content-type", content_type)
     self.end_headers()
 
-  def throw_custom_error(self, message):
-    self._set_response("application/json")
+  def throw_custom_error(self, message,status_code=400):
+    self._set_response("application/json", status_code)
     self.wfile.write(json.dumps({"message": message}).encode())
 
   def do_GET(self):
@@ -32,30 +43,21 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
     global contador
 
-    # Check if action and quantity are present
-    if (body_json.get('action') is None or body_json.get('quantity') is None):
-      self.throw_custom_error("Missing action or quantity")
+    # Check if text field is present
+    if (body_json.get('text') is None):
+      self.throw_custom_error("Missing text field")
       return
 
-    # Check if action is valid
-    if (body_json['action'] != 'asc' and body_json['action'] != 'desc'):
-      self.throw_custom_error("Invalid action")
-      return
+    url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+    payload = {"q": body_json["text"], "target": "es", "source": "en"}
+    headers = {"content-type": "application/x-www-form-urlencoded", "Accept-Encoding": "application/gzip", "X-RapidAPI-Key": X_RAPIDAPI_KEY, "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"}
+    response = requests.post(url, data=payload, headers=headers)
 
-    # Check if quantity is valid, integer
-    try:
-        int(body_json['quantity'])
-    except:
-        self.throw_custom_error("Invalid quantity")
-        return
-
-    if (body_json['action'] == 'asc'):
-        contador += int(body_json['quantity'])
-    elif (body_json['action'] == 'desc'):
-        contador -= int(body_json['quantity'])
-
+    #Translated text
+    texto_traducido = response.json()["data"]["translations"][0]["translatedText"]
+    
     # Respond to the client
-    response_data = json.dumps({"message": "Received POST dataS, new value:" + str(contador), "status": "OK"})
+    response_data = json.dumps({"translatedText": texto_traducido})
     self._set_response("application/json")
     self.wfile.write(response_data.encode())
 
